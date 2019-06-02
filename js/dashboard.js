74b2e20ca86fd60bc98ebc67d0ed6c1c8bdbc165
Saharean <dajspama@gmail.com>
2019-06-02 02:31:16 +0200

    Added transaction display

--- js/dashboard.js
+++ js/dashboard.js
@@ -19,3 +19,40 @@ menuIcon[0].addEventListener('click', function() {
 sidenavClose[0].addEventListener('click', function() {
     toggleClassName(sidenav[0], 'active');
 });
+
+
+function transactionHistory(){
+    var login = "sarian";
+    var request = new XMLHttpRequest();
+    request.open('GET', 'https://192.168.1.40:8080/users/'+ login + '/transactions', true);
+    request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+          // Success!
+          var data = JSON.parse(request.responseText);
+          var list = document.getElementById("av-history");
+        //   for (property in data){
+            for( i=0; i<data.length; i++){
+                list.innerHTML += `<div class="card">
+                <div class="card_transaction-dateblock">
+                    <p class="card_transaction-date">${data[i].orderDate.substr(0,10)}</p>
+                    <p class="card_transaction-type">${data[i].type}</p>
+                </div>
+                <p class="card_transaction-origin">${data[i].fromId}</p>
+                <p class="card_transaction-title">${data[i].title}</p>
+                <div class="card_transaction-amount green">${data[i].money}₱</div>
+            </div>`
+                "<li>" +  + "  " + data[i].fromId
+                + "<br>" + data[i].money + "   " + data[i].title + "</li>" ;
+            }       
+        } else {
+          // We reached our target server, but it returned an error
+      
+        }
+      };
+      
+      request.onerror = function() {
+        // There was a connection error of some sort
+      };
+      
+      request.send();
+}
