3f88d52b47d20a9b1b7397daf61fb8bc0f8c038b
Maciej S <starwars.maciek@gmail.com>
2019-06-12 03:40:21 +0200

    Fixed nfc_card api calls

--- js/dashboard.js
+++ js/dashboard.js
@@ -25,11 +25,36 @@ function transactionHistory(){
     var login = "SaronTetra";
     var account = "66619871643175169398274878";
     var request = new XMLHttpRequest();
-    request.open('GET', 'https://ampervoltapi.putelita.pl:8080/users/'+ login + '/' + account + '/transactions', true);
+    var request2 = new XMLHttpRequest();
+
+    request.open('GET', 'https://ampervoltapi.putelita.pl:8080/users/'+ login + '/' + account + '/', true);
     request.onload = function() {
         if (request.status >= 200 && request.status < 400) {
           // Success!
           var data = JSON.parse(request.responseText);
+          var acc = document.getElementById("av-account");
+          var bal = document.getElementById("av-balance");
+        //   for (property in data){
+            console.log(JSON.stringify(data,null,4));
+            console.log("HELO");
+            
+                acc.innerHTML = `${data.number.replace(/\B(?=(\d{4})+(?!\d))/g, " ")}`;
+                bal.innerHTML = `${data.balance}₱`;
+                  
+        } else {
+          // We reached our target server, but it returned an error
+      
+        }
+    }
+
+    request.send();
+
+    
+    request2.open('GET', 'https://ampervoltapi.putelita.pl:8080/users/'+ login + '/' + account + '/transactions', true);
+    request2.onload = function() {
+        if (request2.status >= 200 && request2.status < 400) {
+          // Success!
+          var data = JSON.parse(request2.responseText);
           var list = document.getElementById("av-history");
         //   for (property in data){
             console.log(JSON.stringify(data,null,4));
@@ -45,7 +70,7 @@ function transactionHistory(){
                     <p class="card_transaction-date">${data[i].orderDate.substr(0,10)}</p>
                     <p class="card_transaction-type">${data[i].type.name}</p>
                 </div>
-                <p class="card_transaction-origin">${data[i].target}</p>
+                <p class="card_transaction-origin">${data[i].target.replace(/\B(?=(\d{4})+(?!\d))/g, " ")}</p>
                 <p class="card_transaction-title">${data[i].title}</p>
                 <div class="card_transaction-amount ${moneyColor}">${data[i].money}₱</div>
             </div>`
@@ -58,9 +83,10 @@ function transactionHistory(){
         }
       };
       
-      request.onerror = function() {
+      request2.onerror = function() {
         // There was a connection error of some sort
       };
       
-      request.send();
+      request2.send();
+    
 }
--- js/nfc_card.js
+++ js/nfc_card.js
@@ -8,111 +8,132 @@ var pin = "";
 
 
 function consoleLog(data) {
-  var logElement = document.getElementById('log');
-  logElement.innerHTML += '\n' + data;
+    var logElement = document.getElementById('log');
+    logElement.innerHTML += '\n' + data;
 }
 
 function clearLog() {
-  var logElement = document.getElementById('log');
-  logElement.innerHTML = '';
+    var logElement = document.getElementById('log');
+    logElement.innerHTML = '';
 }
 
 
 function processPayment(uuid, pin) {
-  var login = document.getElementById("user").value;
-  var title = "Card payment to: " + login;
+    var login = document.getElementById("user").value;
+    var title = "Card payment to: " + login;
+    var account = "";
+    console.log(login);
+    var request1 = new XMLHttpRequest();
+    request1.open('GET', 'https://ampervoltapi.putelita.pl:8080/users/' + login + '/accounts', true);
+    console.log("d23e2");
+    request1.onload = function () {
+        if (request1.status >= 200 && request1.status < 400) {
+            // Success!
+            var data = JSON.parse(request1.responseText);
+            account = data[0].number;
+            console.log("account:" + account);
 
-  var request = new XMLHttpRequest();
-  request.open('POST', 'https://ampervolt.putelita.pl:8080/users/'+ login + '/pay', true);
-  request.setRequestHeader('Content-Type', 'application/json');
-  request.send(JSON.stringify({
-    "uuid": uuid,
-    "money": amount,
-    "title": title
-  }));
+            var request = new XMLHttpRequest();
+            request.open('POST', 'https://ampervoltapi.putelita.pl:8080/users/' + account + '/pay', true);
+            request.setRequestHeader('Content-Type', 'application/json');
+            request.send(JSON.stringify({
+                "uuid": uuid,
+                "money": amount,
+                "title": title
+            }));
+
+        } else {
+            // We reached our target server, but it returned an error
+
+        }
+    }
+    request1.send();
+
+    
 }
 
 function readWriteNfc(pin) {
-  if ('nfc' in navigator) {
-    navigator.nfc.watch(function (message) {
-      var audio = new Audio('../confirm.mp3');
-      audio.play();
-      processPayment(message.records[0].data, pin);
-      navigator.nfc.cancelWatch();
-    }, { mode: 'any' })
-      .then(() => consoleLog("Added a watch."))
-      .catch(err => consoleLog("Adding watch failed: " + err.name));
-  } else {
-    consoleLog('NFC API not supported.');
-  }
+    if ('nfc' in navigator) {
+        navigator.nfc.watch(function (message) {
+            var audio = new Audio('../confirm.mp3');
+            audio.play();
+            processPayment(message.records[0].data, pin);
+            navigator.nfc.cancelWatch();
+        }, { mode: 'any' })
+            .then(() => consoleLog("Added a watch."))
+            .catch(err => consoleLog("Adding watch failed: " + err.name));
+    } else {
+        consoleLog('NFC API not supported.');
+    }
 }
 
-function pinpad_menu(){
-  number = this.querySelectorAll(".pinpad-key-gradient-corner")[0].value;
-  console.log("state:" + state);
-  if(number == "o"){
-    state++;
-  } 
-
-  if(number == "X") { 
-    state = 0;
-    amount = "";
-    pin = "";
-    screen.innerHTML = "";
-  }
-  else {
-    switch (state) {
-      case 0:
-      amount += number;
-      console.log(amount.length);
-        screen.innerHTML = "Amount: <br>"
-        if(amount.length < 3){          
-          screen.innerHTML += "0." + (amount.length = 2 ? amount : "0" + amount) + "₱";
-        }
-        else{
-          screen.innerHTML += amount.substr(0,amount.length-2) + "." + amount.substr(-2,2)  + "₱";
-        }
-        break;
-      case 1:
-        if(amount > 5000){ // TODO: make double
-          state = 2;
-          screen.innerHTML = "Enter PIN: <br>";
-        }
-        else{
-          readWriteNfc(pin);
-          state = 4;
-        }
-        break;
-      case 2:
-      screen.innerHTML = "Enter PIN: <br>";
-        if(pin.length < 4){
-          pin += number;
-        }
-        for (let i = 0; i < pin.length; i++) {
-          screen.innerHTML += "*";        
-        }
-        break;
-      case 3:
-        if(pin.length < 4){
-          state = 2;
-          break;
-        }
-        readWriteNfc();
+function pinpad_menu() {
+    number = this.querySelectorAll(".pinpad-key-gradient-corner")[0].value;
+    console.log("state:" + state);
+    if (number == "o") {
         state++;
-        break;
-      case 5:
+    }
+
+    if (number == "X") {
         state = 0;
         amount = "";
-        pin = ""
-        break;
-      default:
-        break;
+        pin = "";
+        screen.innerHTML = "";
     }
-  }
-  
+    else {
+        switch (state) {
+            case 0:
+                amount += number;
+                console.log(amount.length);
+                screen.innerHTML = "Amount: <br>"
+                // if(amount.length < 3){          
+                //   screen.innerHTML += "0." + (amount.length = 2 ? amount : "0" + amount) + "₱";
+                // }
+                // else{
+                // screen.innerHTML += amount.substr(0, amount.length - 2) + "." + amount.substr(-2, 2) + "₱";
+                // }
+                screen.innerHTML += amount + "₱";
+                break;
+            case 1:
+                if (amount > 5000) { // TODO: make double
+                    state = 2;
+                    screen.innerHTML = "Enter PIN: <br>";
+                }
+                else {
+                    readWriteNfc(pin);
+                    state = 4;
+                }
+                break;
+            case 2:
+                screen.innerHTML = "Enter PIN: <br>";
+                if (pin.length < 4) {
+                    pin += number;
+                }
+                for (let i = 0; i < pin.length; i++) {
+                    screen.innerHTML += "*";
+                }
+                break;
+            case 3:
+                if (pin.length < 4) {
+                    state = 2;
+                    break;
+                }
+                readWriteNfc();
+                state++;
+                break;
+            case 5:
+                state = 0;
+                amount = "";
+                pin = ""
+                break;
+            default:
+                break;
+        }
+    }
+
 }
 
-for(i=0; i<buttons.length; i++){
-  buttons[i].addEventListener('click', pinpad_menu);
+for (i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', pinpad_menu);
 }
 
